367b2fcd54e2d58cbba2aadb4af480684da58529
siah <i@siah.sg>
2026-02-11 13:16:05 +0800

    feat: text not selectable

--- app/dining-rsvp/page.tsx
+++ app/dining-rsvp/page.tsx
@@ -5,7 +5,7 @@ export default function DiningRSVP() {
   return (
     <div className="min-h-screen h-screen w-screen bg-[#88E2FD]">
       <SceneLoader />
-      <div className="font-sans absolute top-1/2 left-1/2 -translate-1/2 bg-white p-2 text-2xl">
+      <div className="font-sans absolute top-1/2 left-1/2 -translate-1/2 bg-white p-2 text-2xl pointer-events-none">
         2月20日より予約受付開始いたします
       </div>
       <div className="hidden">
