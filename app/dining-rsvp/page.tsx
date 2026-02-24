3b0922266f92d5c4fbbab1c9561b17b49167e10c
siah <i@siah.sg>
2026-02-24 18:08:14 +0800

    feat: lol date

--- app/dining-rsvp/page.tsx
+++ app/dining-rsvp/page.tsx
@@ -6,7 +6,7 @@ export default function DiningRSVP() {
     <div className="min-h-screen h-screen w-screen bg-[#88E2FD]">
       <SceneLoader />
       <div className="font-sans absolute top-1/2 left-1/2 -translate-1/2 bg-white p-2 text-2xl pointer-events-none">
-        2月30日より予約受付開始いたします
+        2月28日より予約受付開始いたします
       </div>
       <div className="hidden">
         <RSVPForm />
