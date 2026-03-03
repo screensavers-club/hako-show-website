d4e99a805689e8624114a39f4636220c1ca8636c
siah <i@siah.sg>
2026-03-03 18:25:52 +0800

    feat: modified price

--- app/dining-rsvp/MenuPopover.tsx
+++ app/dining-rsvp/MenuPopover.tsx
@@ -47,7 +47,7 @@ export default function MenuPopover() {
                   <br />
                   コースメニュー
                   <br />
-                  ¥10,700 / おひとり様
+                  ¥11,000 / おひとり様
                 </h1>
               </div>
 
